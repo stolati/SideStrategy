59e34761df4df585060debb4a1d30460f8c1934b
Stolati <mickael.kerbrat@gmail.com>
2014-09-03 23:29:30 +0200

    thoughts about gameplay

---
+++ src/units.py
@@ -0,0 +1,39 @@
+#
+
+
+# This is more informations about gameplay
+
+
+# sky => fast, visible, visibility, fragile
+# earth => slow, unvisible, no-visibility, solid
+# ground => mix between them, more damages
+# water => limited, 
+
+
+
+# humains (everything is round and can be anything)
+# build element that can change into anything
+# there is a basic element, and we can build them with addons
+# so it can fly, go underground or whatewer
+# when they are building, we can choose which part to add, and it give a cost
+# It's slow to begin, but their elements can be devastating once enough technology have been developped.
+
+# it feed on eath elements and eletricity (ho to get them)
+
+
+
+# magiciens (with dinosaurs !!!)
+## - instead of technology, it's spell learned
+# the kind of dinausor give the ability (flying, underground or ground) 
+# Spell : magic missile, teleportation
+
+# Dinausor can feed on other dinausor to get better power/mana/life
+# or resplendish their live (multiples dinosaure)
+
+
+# plants (grow)
+# the mothership is a growing tree and get the energy from the floor (more place is better)
+# we can use energy to expand the roots.
+# Put seed on the floor that can become other things.
+
+# In other words, terran, zerg and protos, yes, my strategy game culture is limited to that
